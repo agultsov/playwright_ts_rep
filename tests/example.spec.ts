83d4d7a429e3117afe9c96c76324f24793104b6c
agultsov <agultsov@elinext.com>
2025-10-06 14:54:24 +0300

    Added codegen test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,51 +6,81 @@ const urlToNavigate: string = 'http://172.16.0.117:8080/ui/login';
 const userName: string = 'tester_user2';
 const userPass: string = 'Eclipse123';
 
-test('Log in/out', async ({ page }, testInfo) => {
-  test.skip(true,'Skipping on the Gitnub Actions');
-  await page.goto(urlToNavigate);
+async function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+}
 
-  await page.locator('css=input[formcontrolname="username"]').fill(userName);
-  await page.locator('css=input[formcontrolname="password"]').fill(userPass);
-  await page.locator('css=button[type="submit"]').click();
-  await page.getByRole('button', {name:"Login"}).click();
-  const newProjectBtn: Locator = page.locator('xpath=//button[child::span[contains(text(),"New Project")]]');
-  await expect(newProjectBtn).toBeVisible({timeout: 15 * 1000});
-  await newProjectBtn.click();
-  await page.waitForTimeout(500);
-  // await page.screenshot({path:'test_log_in.png', fullPage: true});
-
-  const submitBtnPng: Buffer = await page.screenshot({ fullPage: true});
-
-  await testInfo.attach('Clicked New Project button', {
-        body: submitBtnPng,
-        contentType: 'image/png',
+test.describe('Login tests', async () => {
+    test.beforeAll(async () => {
+        await console.info('beforeAll');
     });
 
-  await page.waitForTimeout(1500)
-  await page.close()
-});
+    test.beforeEach(async () => {
+        await console.info('beforeEach');
+    });
+
+    test('Log in/out', async ({page}, testInfo) => {
+        // test.skip(true, 'Skipping on the Gitnub Actions');
+        await page.goto(urlToNavigate);
+
+        await page.locator('css=input[formcontrolname="username"]').fill(userName);
+        await page.locator('css=input[formcontrolname="password"]').fill(userPass);
+        await page.locator('css=button[type="submit"]').click();
+        await page.getByRole('button', {name: "Login"}).click();
+        const newProjectBtn: Locator = page.locator('xpath=//button[child::span[contains(text(),"New Project")]]');
+        await expect(newProjectBtn).toBeVisible({timeout: 15 * 1000});
+        await newProjectBtn.click();
+        await page.waitForTimeout(500);
+        // await page.screenshot({path:'test_log_in.png', fullPage: true});
+
+        const submitBtnPng: Buffer = await page.screenshot({fullPage: true});
+
+        await testInfo.attach('Clicked New Project button', {
+            body: submitBtnPng,
+            contentType: 'image/png',
+        });
+
+        await page.waitForTimeout(1500)
+        await page.close()
+    });
+
+    test('Log in/out by playwright codegen', async ({ page }) => {
+        await page.goto(urlToNavigate);
+        await page.getByRole('textbox', { name: 'Username' }).click();
+        await page.getByRole('textbox', { name: 'Username' }).fill(userName);
+        await page.getByRole('textbox', { name: 'Password' }).click();
+        await page.getByRole('textbox', { name: 'Password' }).fill(userPass);
+        await page.getByRole('button', { name: 'Login' }).click();
+        await page.getByRole('button', { name: 'New Project' }).click();
+        await page.getByText('Internal and External').first().click();
+        await page.locator('div').filter({ hasText: /^Measure Risk$/ }).click();
+        await page.getByRole('button', { name: 'Next' }).click();
+        await page.getByRole('textbox', { name: 'Project Name' }).fill(`alexProj_${await getRandomInt(4000)}`);
+        await page.getByRole('button', { name: 'Save' }).click();
+        await page.getByRole('button', { name: 'T tester_user2' }).click();
+        await page.getByRole('menuitem', { name: 'Logout' }).click();
+    });
 
 
-test('Check Facebook', async ({ page }, testInfo) => {
-  await page.goto(urlToNavigateFB);
+    test('Check Facebook', async ({page}, testInfo) => {
+        await page.goto(urlToNavigateFB);
 
 
-  await page.getByTestId('royal-email').fill(userName);
-  await page.getByTestId('royal-pass').fill(userPass);
+        await page.getByTestId('royal-email').fill(userName);
+        await page.getByTestId('royal-pass').fill(userPass);
 
 
-  // Expect a title "to contain" a substring.
-  const LogInBtn: Locator = page.getByTestId('royal-login-button');
-  await expect(LogInBtn).toBeVisible({timeout: 15 * 1000});
+        // Expect a title "to contain" a substring.
+        const LogInBtn: Locator = page.getByTestId('royal-login-button');
+        await expect(LogInBtn).toBeVisible({timeout: 15 * 1000});
 
-  const submitBtnPng: Buffer = await page.screenshot({ fullPage: true});
+        const submitBtnPng: Buffer = await page.screenshot({fullPage: true});
 
-  await testInfo.attach('Facebook screen', {
-    body: submitBtnPng,
-    contentType: 'image/png',
-});
-});
+        await testInfo.attach('Facebook screen', {
+            body: submitBtnPng,
+            contentType: 'image/png',
+        });
+    });
 
 // test('get started link', async ({ page }) => {
 //   await page.goto(urlToNavigate);
@@ -66,3 +96,4 @@ test('Check Facebook', async ({ page }, testInfo) => {
 //   await expect(page.locator('text=Download CV')).toBeVisible({timeout: 35 * 1000});
 //   // await expect(page.getByRole('button', { name: 'Download CV' })).toBeVisible({timeout: 15 * 1000});
 // });
+});
